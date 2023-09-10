843c66a49f7d7a3e4444303191b98e95edf88862
Marconi Gomes <marconi.gomes7@gmail.com>
2023-09-09 22:37:40 -0300

    chore: Bumps tag

--- AMRPC WatchDog Desktop/Program.cs
+++ AMRPC WatchDog Desktop/Program.cs
@@ -6,7 +6,7 @@ namespace AMRPC_WatchDog_Desktop
     internal static class Program
     {
 
-        private const string AppVersion = "v0.0.1"; 
+        private const string AppVersion = "v0.1.0"; 
         [STAThread]
         private static void Main()
         {
